4d6c8ac0b037db0f4226da47917c3c9dd05d2830
Pushkarev Anton <pushkarevantona@gmail.com>
2022-01-07 22:31:28 +0300

    Automatic login after SignUp added

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -8,3 +10,13 @@ class SignUp(CreateView):
     form_class = CreationForm
     template_name = 'users/signup.html'
     success_url = reverse_lazy('posts:index')
+
+    def form_valid(self, form):
+        """Автоматический вход для зарегистрированного пользователя."""
+        form.save()
+        user = authenticate(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+        )
+        login(self.request, user)
+        return redirect(self.success_url)
